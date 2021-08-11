3ef930f308887484ca6cf7579d25a19e02de5e40
Aakash Thapa <aakashmanm3@gmail.com>
2021-08-11 06:41:51 +0545

    Hero Section Added

--- static/js/main.js
+++ static/js/main.js
@@ -2,8 +2,17 @@
 (function () {
     var scroll = new LocomotiveScroll({
         smooth: true,
-        multiplier: 0.4
+        multiplier: 0.6
     });
 })();
+function heroSectionOpAnimation(){
+    // gsap.from(".HeroName", {y:10, duration:1,opacity:0})
+    let rule = CSSRulePlugin.getRule(".heroSection .wrapper .heroContent .HeroWork h1 span::after")
+    gsap.to(rule, {cssRule: {scaleY: 0}, duration: 1});
+    gsap.from(".heroSection .wrapper .heroContent .hero-item", {y:50, duration: 1, opacity:0,stagger: .5})
+}
 
+window.addEventListener("load", () => {
+    heroSectionOpAnimation()
 
+})
