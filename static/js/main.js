812bf6187ae9981b53c0a31819d84f81d681401c
Aakash Thapa <aakashmanm3@gmail.com>
2021-08-12 11:42:57 +0545

    Some animation added

--- static/js/main.js
+++ static/js/main.js
@@ -12,7 +12,31 @@ function heroSectionOpAnimation(){
     gsap.from(".heroSection .wrapper .heroContent .hero-item", {y:50, duration: 1, opacity:0,stagger: .5})
 }
 
+function HeroIdeasAnimation(){
+    let heroIdeas =document.querySelectorAll(".heroContent .heroIdeas ul li");
+    let heroIdeasSection = document.querySelector(".heroContent .heroIdeas ul");
+    let slide = 0
+    let slided = 32
+    setInterval(() => {
+        slide -= slided;
+        if (slide <= -64){
+            slided = -slided
+        }
+        else if(slide >= 0){
+            slided = -slided
+        }
+        for(i = 0; i < heroIdeas.length; i++){
+
+            gsap.to(heroIdeasSection, {top: slide, duration: 1})
+            gsap.from(heroIdeas[i], {opacity: 0})
+
+
+        }
+    }, 3000);
+
+}
 window.addEventListener("load", () => {
     heroSectionOpAnimation()
+    HeroIdeasAnimation()
 
 })
